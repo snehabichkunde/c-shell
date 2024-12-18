cdbc4f0aa0b6ce1ff1f60275662606de62a6a6d4
snehabichkunde <bichkundesneha@gmail.com>
2024-12-18 21:16:21 +0530

    Implement ls command, right/left arrows, and backspace (work in progress)

--- main.c
+++ main.c
@@ -6,13 +6,18 @@
 #include<ncurses.h>
 #include<unistd.h>
 #include<sys/wait.h>
+#include <curses.h>
 
 #define ctrl(x) ((x)& 0x1f)
 #define SHELL "[c-shell]~ "
 #define ENTER 10
 #define UP_ARROW 259
 #define DOWN_ARROW 258
-
+#define LEFT_ARROW 260
+#define RIGHT_ARROW 261
+#ifndef KEY_BACKSPACE
+#define KEY_BACKSPACE 127
+#endif
 
 #define DATA_START_CAPACITY 128 // Initial capacity for dynamic arrays
 
@@ -59,32 +64,70 @@ typedef struct
     size_t capacity;    
 }Strings;
 
+// Function declarations
+void handle_command(char **args, size_t *line);
+char *str_to_cstr(String str);
+char **parse_command(char *command);
 
-void clear_line(int line) {
-    move(line, 0);  // Move cursor to the start of the line
-    clrtoeol();     // Clear everything to the end of the line
+
+void clear_line(size_t line, size_t width) {
+    for (size_t i = 0; i < width; i++) {
+        mvprintw(line, i, " ");  // Clear the entire line by printing spaces
+    }
 }
 
-void handle_command(char *file, char **args)
-{
-	int status;
-	int pid = fork();
-	if(pid < 0) { // error
-		fprintf(stderr, "error %s", strerror(errno));
-		return;
-	} else if(!pid) { // child process
-		if(execvp(args[0], args) < 0) {
-			fprintf(stderr, "%s\n", strerror(errno));
-		}
-		exit(1);
-	} else { // parent process
-		int wpid = waitpid(pid, &status, 0);
-		while(!WIFEXITED(status) && !WIFSIGNALED(status)) {
-			wpid = waitpid(pid, &status, 0);
-		}
-		(void)wpid;
-	}	
+
+void handle_command(char **args, size_t *line) {
+    char buf[4096] = {0};
+
+    int filedes[2];
+    if (pipe(filedes) < 0) {
+        mvprintw(*line, 0, "error %s", strerror(errno));
+        return;
+    }
+
+    int status;
+    int pid = fork();
+
+    if (pid < 0) { // error
+        mvprintw(*line, 0, "error %s", strerror(errno));
+        return;
+    } else if (!pid) { // child process
+        close(filedes[0]);
+        if (dup2(filedes[1], STDOUT_FILENO) < 0) {
+            printf("%s\n", strerror(errno));
+        }
+        close(filedes[1]);
+
+        if (execvp(args[0], args) < 0) {
+            printf("%s\n", strerror(errno));
+        }
+        exit(1);
+    } else { // parent process
+        close(filedes[1]);
+
+        int nbytes;
+        while ((nbytes = read(filedes[0], buf, sizeof(buf) - 1)) != 0) {
+            mvprintw(*line, 0, "%s", buf);
+            for (size_t i = 0; buf[i] != '\0'; i++) {
+                if (buf[i] == '\n') {
+                    *line += 1;
+                }
+            }
+            refresh();
+            memset(buf, 0, sizeof(buf));
+        }
+
+        int wpid = waitpid(pid, &status, 0);
+        while (!WIFEXITED(status) && !WIFSIGNALED(status)) {
+            wpid = waitpid(pid, &status, 0);
+        }
+        close(filedes[0]);
+        refresh();
+    }
 }
+
+
 
 
 char *str_to_cstr(String str)
@@ -95,25 +138,28 @@ char *str_to_cstr(String str)
     return cstr;
 }
 
-char ** parse_command(String command){
-    char *cmd_cstr = str_to_cstr(command);
-    char *cur = strtok(cmd_cstr, " ");
-    if(cur == NULL){
-        //free(cmd_cstr);
+char **parse_command(char *command) {
+    char *cur = strtok(command, " ");
+    if (cur == NULL) {
         return NULL;
     }
+
     size_t args_s = 8;
-    char ** args = malloc(sizeof(char*)*args_s);
+    char **args = malloc(sizeof(char*) * args_s);
     size_t args_cur = 0;
-    while(cur!= NULL){
-        if(args_cur >= args_s){
+
+    while (cur != NULL) {
+        if (args_cur >= args_s) {
             args_s *= 2;
-            args = realloc(args, sizeof(char*)*args_s);
+            args = realloc(args, sizeof(char*) * args_s);
         }
+
         args[args_cur++] = cur;
         cur = strtok(NULL, " ");
     }
-    
+
+    args[args_cur] = NULL;
+
     return args;
 }
 
@@ -129,12 +175,20 @@ int main()
     Strings command_his = {0};
     DA_APPEND(&command_his, command);
     
+    int command_pos = 0;  
 
     bool QUIT = false; 
     size_t line = 0;
+    size_t height = 0;
+    size_t width = 0;
     while(!QUIT){
+        getmaxyx(stdscr, height, width);
+        clear_line(line, width);
         mvprintw(line, 0, SHELL);
         mvprintw(line , 0 + sizeof(SHELL) - 1, "%.*s", (int)command.count, command.data);
+
+        move(line, sizeof(SHELL)-1+command_pos);
+
         ch = getch();
         switch(ch){
             case ctrl('q'):  // quit 
@@ -143,45 +197,71 @@ int main()
 
             case ENTER: // enter 
                 line++;  
-                clear_line(line);    
-                char **args = parse_command(command);
-                line++; 
-                handle_command(args[0], args);        
-                DA_APPEND(&command_his, command);
+                clear_line(line, width); 
+                command_pos = 0;  
+                char **args = NULL;  
+                if(command.count> 0){
+                    args = parse_command(str_to_cstr(command));
+                }
+                mvprintw(line, command.count, "\n\r");
+                if(args!=NULL){
+                    handle_command(args, &line);        
+                    DA_APPEND(&command_his, command);
+                    free(args);
+                }
                 command = (String){0};  // Reset command
                 history_index = -1;  // Reset the history index
                 break;
-            
+            case ctrl('c'):
+                line++;
+                command = (String){0}; 
+                break;
+            case KEY_BACKSPACE:
+            case 8:
+                if(command.count > 0) command.data[--command.count] = '\0';
+                break;
+            case LEFT_ARROW:
+                if(command_pos > 0) {
+                command_pos--;
+                }
+                break;
+            case RIGHT_ARROW:
+                if(command_pos < command.count) {
+                    command_pos++;
+                }
+                break;
             case UP_ARROW:
                 if (command_his.count > 0) {
                     if (history_index == -1) {
                         // Start at the last history command
                         history_index = 0;
                     } else if (history_index < (int)command_his.count - 1) {
-                    // Move one step back in history
-                    history_index++;
+                        // Move one step back in history
+                        history_index++;
+                    }
+                    // Fetch the command from history
+                    clear_line(line, width);  // Clear the current line
+                    command = command_his.data[command_his.count - 1 - history_index];
+                    command_pos = command.count;
                 }
-
-        // Fetch the command from history
-        clear_line(line); // Clear the current line
-        command = command_his.data[command_his.count - 1 - history_index];
-    }
                 break;
 
             case DOWN_ARROW:
                 if (history_index > -1) {
                     history_index--;
-                    clear_line(line);
+                    clear_line(line, width); 
                     command = command_his.data[command_his.count - 1 - history_index];
                 } else {
-                    clear_line(line);
+                    clear_line(line, width); 
                     command = (String){0};  // Clear the current command when reaching the newest one
                 }
+                command_pos = command.count;
                 break;
             
 
             default: 
                 DA_APPEND(&command, ch);
+                command_pos++;
                 break;
         }
     }
